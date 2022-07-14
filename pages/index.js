580ae394eb6ab9334669359dd2bf73b0ce87233e
Joshua LaMorey-Salzmann <lamorey.joshua@gmail.com>
2022-07-14 16:56:26 +0200

    fifth commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import image from "../public/berlin-rbb24.jpg";
 
 export default function Home() {
   return (
@@ -10,20 +11,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 classname="color1">My Personal Page</h1>
+      <h1 className="color1">My Personal Page</h1>
       <br> </br>
       <h2>
-        <span classname="color2">Hi,</span>
+        <span className="color2">Hi,</span>
         <span>I&apos;m Josh 👋</span>
       </h2>
       <h3>I&apos;m 36 years old.</h3>
       <br></br>
       <h3>Here&apos;s a photo of the city where I live. 🖼️</h3>
-      <Image
-        src="https://www.rbb24.de/content/dam/rbb/rbb/rbb24/2022/2022_03/dpa-account/berlin1.jpg.jpg/size=708x398.jpg"
-        width="700"
-        alt="Birds-eye view of Berlin"
-      />
+      <div>
+        <img src={image} alt="Grossstadt" />
+      </div>
 
       <p>
         I&apos;ve lived in Berlin for 4 years. It&apos;s one of two German
@@ -32,9 +31,9 @@ export default function Home() {
       </p>
 
       <section>
-        <h2 classname="color1">Hobbies</h2>
+        <h2 className="color1">Hobbies</h2>
         <p>These are some things I enjoy.</p>
-        <ul classname="color2">
+        <ul className="color2">
           <li>Cooking food</li>
           <li>Trying to practice yoga</li>
           <li>Reading newspapers and books</li>
